eb2369547222a9e471844c48fdb776599b70fb0f
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2021-05-08 13:24:29 -0400

    Adjust HexagonGrid naming convention

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <vue-hexagon text="Hello World!"></vue-hexagon>
+    <vue-hexagon color="blue">
+      <span slot="top">8</span>
+      <span slot="middle">Oxygen</span>
+      <span slot="bottom">O2</span>
+    </vue-hexagon>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
